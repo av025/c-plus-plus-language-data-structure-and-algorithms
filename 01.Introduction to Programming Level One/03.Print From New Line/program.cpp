9a3dc34dd6824d76a18d93ee409b6dd9d195cd99
Aayush Vyas <aayush25vyas@gmail.com>
2025-07-04 23:05:17 +0530

    endl and new line character in c++

---
+++ 01.Introduction to Programming Level One/03.Print From New Line/program.cpp
@@ -0,0 +1,17 @@
+#include <iostream> 
+
+using namespace std; 
+
+int main() {
+    
+    //* Now we want to print something from new line 
+    //* There was two methods to print from new line 
+    //? 1st. endl : endl stands for end of line this statement helps to end the current line and start execution from the new line.
+    cout << "Aayush";
+    cout << endl; 
+    cout << "Vyas" << endl; 
+    //* Together in same line we also used this endl statement 
+    //? 2nd. '\n' : new line character that also used to end the current line and start execution from the new line 
+    cout << "Kratik" << '\n'; 
+    cout << "Vyas" << "\n";
+}
