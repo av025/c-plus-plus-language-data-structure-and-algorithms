980cefd80f030e843ca50ca9ac705459e217ee15
Aayush Vyas <aayush25vyas@gmail.com>
2025-07-04 23:47:22 +0530

    Comments in C++

--- 01.Introduction to Programming Level One/02. Hello World Program in c++/program.cpp
+++ 01.Introduction to Programming Level One/02. Hello World Program in c++/program.cpp
@@ -3,4 +3,7 @@ using namespace std;
 
 int main() {
     cout << "Hello World"; 
+
+    return 0;  //* Here we returning the 0 which indicate the Operating system that our program successfully executed. 
+    // return 1;  //* Here we returning 1 which indicate OS that we get error at the time of execution to our program
 }
