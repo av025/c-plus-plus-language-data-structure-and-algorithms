4d2c39e26504dcba88a39466415ba9106be6516e
Aayush Vyas <aayush25vyas@gmail.com>
2025-07-04 23:48:04 +0530

    changes in a file

---
+++ 01.Introduction to Programming Level One/04. Comments in c++/program.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std; 
+
+int main() {
+    //* Comments 
+    // comments are also important features of programming language it helps to take notes in programming and also to create documentation of our code feature why we create it and what was feature of this implemented code so whenever we work in an organization that comments help us to understand the feature and implementation of the code. 
+
+    // there was two type of comments: 
+    // Single line comment 
+    /**
+     Multi-line 
+     Comments 
+     */
+
+    //  cout << "Aayush" << endl;  We can comment our code also which does not executed on standard output 
+     cout << "Vyas"; 
+    return 0; 
+}
