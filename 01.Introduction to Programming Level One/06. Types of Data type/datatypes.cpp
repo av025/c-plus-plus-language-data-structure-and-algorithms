b7cae7d8f037f9d18fc5b861ad50a8486151cb63
Aayush Vyas <aayush25vyas@gmail.com>
2025-07-05 15:54:22 +0530

    Types of Data type in c++

---
+++ 01.Introduction to Programming Level One/06. Types of Data type/datatypes.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std; 
+
+int main() {
+//? In programming language our datatypes are mainly categorized in two types but in c++ there was categorized in three type
+
+//* 1st. Primitive Data Types
+// Primitive data types are also called in-built data types because they are predefined by the programming language.
+// These data types are used to store simple values such as numbers and characters.
+// Examples: int, char, float, bool, double, long, etc.
+
+//* 2nd. Non-Primitive Data Types
+// Non-primitive data types are also called derived data types because they are derived from primitive types.
+// These types are used to store more complex data structures in a program.
+// Examples: arrays, pointers, strings, etc.
+
+//* 3rd. User-defined Data Types
+// C++ also allows the creation of user-defined data types, which can be used to represent more complex data.
+// These are created by the programmer as per the program’s requirements.
+// Examples: structures (struct), unions (union), classes (class), enumerations (enum), etc. 
+
+
+//! Datatypes give us two most important information : 
+//* 1st. Type of  data we stored it. 
+//* 2nd. Size of memory taken by our datatypes. 
+
+
+
+    return 0; 
+}
