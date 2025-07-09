262e6f690f2b901174c9a379bb988ea97ec9141f
Aayush Vyas <aayush25vyas@gmail.com>
2025-07-09 16:42:25 +0530

    How signed and unsigned data was stored in program memory

--- 01.Introduction to Programming Level One/10.signed and unsigned integers/signAndUnsignIntegers.cpp
+++ 01.Introduction to Programming Level One/10.signed and unsigned integers/signAndUnsignIntegers.cpp
@@ -28,7 +28,40 @@ cout << numberZ << endl;  // Output : 2000
 
 //? What happen when we assign unsigned variable negative value 
 unsigned int numberA = -500; 
-cout << numberA << endl;  //! It prints the garbage value when we assign the negative value. 
+cout << numberA << endl;  //! It prints the garbage value when we assign the negative value.  
+
+
+//? How any integer number will be signed or unsigned will be decide in memory of our program ? 
+//* The integers are positive or negative was find by the most-left bit if the value was 0 than it was positive number and if left-most bit was 1 than it was negative number. 
+
+//* Understand with example 
+int numberTwo = 3;  
+
+//* This above decimal number 3 was stored in binary equivalent which was 11 but we are using int datatype which takes 4 byte of memory space in program. 
+
+//? 00000000 00000000 00000000 00000011   
+//?  1 byte   2 byte   3 byte    4 byte   
+//! Here left-most significant bit was 0 it means it was positive number.   
+
+//? If we want to check if any number was positive than left-most significant bit was 0 and in the case of negative left-most significant bit was 1.   
+
+int numberThree = -3; 
+//* This above decimal number -3 was stored in binary equivalent which was we find with 3 steps 
+//* 1st. ignore the negative sign 
+//* 2nd. find binary equivalent 
+//* 3rd. Find 2's compliement 
+
+//? 11111111 11111111 11111111 11111101 
+//?  1 Byte  2 Byte    3 Byte   4 Byte   
+//! If it was negative number than left-most significant bit was 1.  
+
+//* In memory when we stored our data or value than it's left-most bit was called most-significant bit and sign bit and most-right side bit was called as least-significant bit. 
+
+//! Char datatypes also have signed and unsigned values so if we have to find range of unsigned bit than we have to do half our total characters 256 was there so signed bit range was -128 to 127  and unsigned bit range was 0 to 256 
+
+//* So the generic formula of signed bit was 2**n to 2**n - 1 and unsigned was 0 to 2**n - 1 
+
+
 
 return 0; 
 
